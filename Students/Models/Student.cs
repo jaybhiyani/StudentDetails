5e8d875696b9bd71eb9a8bdf0720aa20b877335a
Jay Bhiyani <jaybhiyani15@gmail.com>
2019-11-02 15:32:16 +0530

    Model=StudentContext, Connection String, EFCore, DbServices Configured

--- Students/Models/Student.cs
+++ Students/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,13 @@ namespace Students.Models
 {
     public class Student
     {
+        [Key]
         public string Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(50)")]
         public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(2)")]
         public string Department { get; set; }
     }
 }
---
+++ Students/Models/StudentContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Students.Models
+{
+    public class StudentContext:DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
