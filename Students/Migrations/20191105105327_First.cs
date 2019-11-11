feea9ee7ba4b445a4111e157b08afdcdca28364d
Jay Bhiyani <jaybhiyani15@gmail.com>
2019-11-11 13:01:02 +0530

    Contoller = StudentsController and database migrations added

---
+++ Students/Migrations/20191105105327_First.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Students.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Department = table.Column<string>(type: "varchar(2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
